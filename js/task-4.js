5b3248367cc2e44cf57fdf1709b65e987ee5cba5
LuKaSzKoWi <l.kowalski8509@gmail.com>
2024-08-17 11:21:42 +0200

    all tasks finished

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+document.querySelector('.login-form').addEventListener('submit', function(event) {
+  event.preventDefault();
 
+  const emailInput = event.target.elements.email.value.trim();
+  const passwordInput = event.target.elements.password.value.trim();
+
+  if (!emailInput || !passwordInput) {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    email: emailInput,
+    password: passwordInput
+  };
+
+  console.log(formData);
+
+  event.target.reset(); 
+});
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,41 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxesContainer = document.querySelector('#boxes');
+  const sizeStep = 10; 
+  const initialSize = 30; 
+
+  
+  boxesContainer.innerHTML = '';
+
+  
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement('div');
+    const size = initialSize + i * sizeStep; 
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(div);
+  }
+}
+
+
+function destroyBoxes() {
+  document.querySelector('#boxes').innerHTML = '';
+}
+
+
+document.querySelector('[data-create]').addEventListener('click', () => {
+  const input = document.querySelector('#controls input');
+  const amount = Number(input.value);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  }
+
+  input.value = '';
+});
+
+document.querySelector('[data-destroy]').addEventListener('click', destroyBoxes);
